318696bff5203bcb62c60348d2d9ede766ba655c
Dennis Ideler <ideler.dennis@gmail.com>
2016-07-30 20:06:26 +0100

    v1.0.0

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function toggleComments() {
+	var label = document.getElementById('toggle-comments').firstElementChild;
+	var comments = document.getElementById('watch-discussion');
+	if (comments.classList.toggle('hide-comments')) {
+		label.textContent = 'Show comments';
+	}	else {
+		label.textContent = 'Hide comments';
+	}
+}
+
+function inject() {
+	addClass();
+	addButton();
+}
+
+function addClass() {
+	document.getElementById('watch-discussion').className += ' hide-comments';
+}
+
+function addButton() {
+	var button =`
+	<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander" id="toggle-comments" type="button">
+		<span class="yt-uix-button-content">Show comments</span>
+	</button>
+	`;
+
+	document.getElementById('action-panel-details').innerHTML += button;
+	document.getElementById('toggle-comments').addEventListener('click', toggleComments);
+}
+
+(function () {
+	document.addEventListener('DOMContentLoaded', inject); // Static navigation (i.e. initial page load)
+	document.addEventListener('spfdone', inject); // Dynamic navigation (i.e. subsequent page loads)
+})();
