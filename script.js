ebb3290aa2788c759af10dd58433175e09173dc0
Dennis Ideler <ideler.dennis@gmail.com>
2017-09-01 23:57:41 +0100

    :green_heart: Quick fix for new YouTube UI
    The new YouTube UI uses Polymer and completely different events than
    the spf events from the previous version.
    
    The comments section is dynamic. It's not loaded until you scroll down
    to the comments section. Which means we have to wait for the section
    to exist before we can inject.
    
    Tried listening to various events but the one that seems to work best is
    'yt-register-action'. The problem with this event is that it's generic
    and is used for many different actions, so we have to check what the
    event fired for.
    
    Extra checks are needed to keep backwards compatibility, as the new UI
    could be a phased rollout, and because you can revert back to the old UI
    via your settings menu.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 'use strict';
 
+let injected = false;
+
+function newToggleComments() {
+	const label = document.getElementById('toggle-comments').firstElementChild;
+	const comments = document.getElementById('comments');
+
+	if (comments.classList.toggle('hide-comments')) {
+		label.textContent = 'Show comments';
+	}	else {
+		label.textContent = 'Hide comments';
+	}
+}
+
 function toggleComments() {
 	const label = document.getElementById('toggle-comments').firstElementChild;
 	const comments = document.getElementById('watch-discussion');
@@ -13,9 +26,32 @@ function toggleComments() {
 	showReadMore();
 }
 
-function inject() {
-	addClass();
-	addButton();
+function inject(e) {
+	if (e.type == 'spfdone') { injected = false; } // reset for old UI navigation
+	if (injected) { return; } // guard clause because event 'yt-register-action' fires many times
+
+	if (isOldInterface()) {
+		addClass();
+		addButton();
+	} else {
+		if (e.target.id != 'comments') {
+			return; // do not inject if triggering too early
+		}
+
+		const oldTriggerEvents = ['DOMContentLoaded', 'spfdone'];
+		if (oldTriggerEvents.includes(e.type)) {
+			return; // do not inject for old UI events on new UI
+		}
+
+		addNewClass();
+		addNewButton();
+	}
+
+	injected = true;
+}
+
+function isOldInterface() {
+	return !!document.getElementById('watch-discussion');
 }
 
 function addClass() {
@@ -31,6 +67,23 @@ function addButton() {
 
 	document.getElementById('action-panel-details').innerHTML += button;
 	document.getElementById('toggle-comments').addEventListener('click', toggleComments);
+}
+
+function addNewClass() {
+	document.getElementById('comments').className += ' hide-comments';
+}
+
+function addNewButton() {
+	const button =`
+	<button class="fake-paper-button" id="toggle-comments" type="button">
+		<span class="fake-yt-formatted-string">Show comments</span>
+	</button>
+	`;
+
+	const moreButton = document.getElementById('more');
+	moreButton.insertAdjacentHTML('afterend', button);
+
+	document.getElementById('toggle-comments').addEventListener('click', newToggleComments);
 }
 
 function showReadMore() {
@@ -49,4 +102,5 @@ function showReadMore() {
 (function () {
 	document.addEventListener('DOMContentLoaded', inject); // Static navigation (i.e. initial page load)
 	document.addEventListener('spfdone', inject); // Dynamic navigation (i.e. subsequent page loads)
+	document.addEventListener('yt-register-action', inject); // Polymer event for new UI
 })();
