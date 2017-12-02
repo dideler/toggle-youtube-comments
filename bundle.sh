3261f6f5f5b40502e568849c52ae1454e07f9fe3
Dennis Ideler <ideler.dennis@gmail.com>
2017-12-02 13:42:25 +0000

    :package: Exclude zip files from bundle
    Do not package older bundles that have been kept around locally, when
    creating a new bundle. Without this you would have to remember to
    manually remove zips before creating a new bundle.

--- bundle.sh
+++ bundle.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-zip -9 -r $(date +%F_%H%M)-toggle-youtube-comments.zip . -x "*.git*" "*.md" "$0"
+zip -9 -r $(date +%F_%H%M)-toggle-youtube-comments.zip . -x "*.git*" "*.md" "*.zip" "$0"
