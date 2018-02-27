bc2d332a0659de79dd40d0d7640799bbdcc354b6
Dennis Ideler <ideler.dennis@gmail.com>
2018-02-27 15:57:35 +0000

    :tshirt: Run Prettier against new JS files
    If we don't like any of these style changes, we can always override them
    via the .prettierrc file. See the available Prettier options at
    https://prettier.io/docs/en/options.html.

--- tasks/compress.js
+++ tasks/compress.js
@@ -1,39 +1,39 @@
-const util = require('util')
-const fs = require('fs')
-const path = require('path')
-const shell = require('shelljs')
-const globby = require('globby')
-const makeDir = require('make-dir')
-const uglifyES = require('uglify-es')
-const imagemin = require('imagemin')
-const imageminPngquant = require('imagemin-pngquant')
-const jsonfile = require('jsonfile')
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+const globby = require('globby');
+const makeDir = require('make-dir');
+const uglifyES = require('uglify-es');
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const jsonfile = require('jsonfile');
 
 /**
  * Using clean-css-cli directly.
  */
 const compressCss = () =>
-  shell.exec('cleancss -O1 ./src/youtube.css -o ./_dist/youtube.css')
+  shell.exec('cleancss -O1 ./src/youtube.css -o ./_dist/youtube.css');
 
 /**
  * Using Uglify-ES on node.js
  */
 const compressJs = () => {
-  const src = fs.readFileSync(path.resolve('./src/script.js'), 'utf8')
-  const dist = path.resolve('./_dist/script.js')
+  const src = fs.readFileSync(path.resolve('./src/script.js'), 'utf8');
+  const dist = path.resolve('./_dist/script.js');
   const options = {
     compress: {
       drop_console: true,
-      ecma: 6
+      ecma: 6,
     },
-    mangle: { toplevel: true }
-  }
+    mangle: { toplevel: true },
+  };
 
-  const result = uglifyES.minify(src, options)
+  const result = uglifyES.minify(src, options);
 
   // Output compressed file.
-  fs.writeFileSync(dist, result.code)
-}
+  fs.writeFileSync(dist, result.code);
+};
 
 /**
  * Using imagemin on node.js
@@ -44,13 +44,13 @@ const compressImage = () => {
       imageminPngquant({
         quality: '75-85',
         speed: 1,
-        verbose: true
-      })
-    ]
+        verbose: true,
+      }),
+    ],
   }).then(files => {
-    console.log('Images optimized')
-  })
-}
+    console.log('Images optimized');
+  });
+};
 
 /**
  * Compress JSON on node.js
@@ -58,55 +58,53 @@ const compressImage = () => {
 const compressJson = async () => {
   const srcPathsArray = await Promise.all([
     globby('src/_locales/**/messages.json'),
-    globby('src/manifest.json')
-  ]).then(arrays => [].concat(...arrays)) // flatten array
+    globby('src/manifest.json'),
+  ]).then(arrays => [].concat(...arrays)); // flatten array
 
   const distPathsArray = await Promise.all(
     srcPathsArray.map(async v => {
-      const distPath = v.replace('src/', '_dist/')
-      await makeDir(path.dirname(distPath)) // make parent directory of file.
-      return path.resolve(distPath) // return absolute path.
+      const distPath = v.replace('src/', '_dist/');
+      await makeDir(path.dirname(distPath)); // make parent directory of file.
+      return path.resolve(distPath); // return absolute path.
     })
-  )
+  );
 
   // Promisify
-  const promiseJsonRead = util.promisify(jsonfile.readFile)
-  const promiseJsonWrite = util.promisify(jsonfile.writeFile)
+  const promiseJsonRead = util.promisify(jsonfile.readFile);
+  const promiseJsonWrite = util.promisify(jsonfile.writeFile);
 
   const readFiles = async filePaths => {
-    const jsonContentArray = filePaths.map(filePath =>
-      promiseJsonRead(filePath)
-    )
-    return Promise.all(jsonContentArray)
-  }
+    const jsonContentArray = filePaths.map(filePath => promiseJsonRead(filePath));
+    return Promise.all(jsonContentArray);
+  };
 
   const writeFiles = async (distPaths, contents) => {
     await Promise.all(
       contents.map((content, i) => {
-        const distPath = distPaths[i]
+        const distPath = distPaths[i];
         const write = promiseJsonWrite(distPath, content, {
           replacer: null,
-          spaces: 0
-        })
+          spaces: 0,
+        });
       })
-    ).then(log => console.log('JSON compressed'))
-  }
+    ).then(log => console.log('JSON compressed'));
+  };
 
-  const jsonContents = await readFiles(srcPathsArray)
+  const jsonContents = await readFiles(srcPathsArray);
 
-  const write = await writeFiles(distPathsArray, jsonContents)
-}
+  const write = await writeFiles(distPathsArray, jsonContents);
+};
 
 /**
  *  Run processes.
  */
-;(async () => {
+(async () => {
   const processes = await Promise.all([
     compressCss(),
     compressJs(),
     compressImage(),
-    compressJson()
+    compressJson(),
   ])
     .then(x => console.log('Compress: Completed'))
-    .catch(err => console.error(err))
-})()
+    .catch(err => console.error(err));
+})();
--- tasks/pack.js
+++ tasks/pack.js
@@ -1,7 +1,7 @@
-const shell = require('shelljs')
-const moment = require('moment')
+const shell = require('shelljs');
+const moment = require('moment');
 
-const timeStamp = moment().format('YYYY-MM-DD_HHmm')
-const fileName = `${timeStamp}-toggle-youtube-comments`
+const timeStamp = moment().format('YYYY-MM-DD_HHmm');
+const fileName = `${timeStamp}-toggle-youtube-comments`;
 
-shell.exec(`zip -9 -r _packages/${fileName}.zip ./_dist/*`)
+shell.exec(`zip -9 -r _packages/${fileName}.zip ./_dist/*`);
